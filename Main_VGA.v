07576e88a09f57c540257f41e17dc8c11ac76a8c
georgeg2cpp <george.gonzalez.ce@gmail.com>
2025-04-28 20:16:36 -0700

    Update Main_VGA.v
    added generate asteroid and bullet

--- Main_VGA.v
+++ Main_VGA.v
@@ -1,47 +1,59 @@
+`timescale 1ns / 1ps
+
 module top_vga(
-			   input wire	     clk, // 100 MHz board clock on Nexys A7
+input wire	     clk, // 100 MHz board clock on Nexys A7
 			   input wire	     rst, // Active-high reset
 			   input wire move_up,
 			   input wire move_down,
 			   input wire move_left,
 			   input wire move_right,
-			   input wire btnC,
+			   input wire shoot,
 			   // VGA outputs
 			   output wire [3:0] vgaRed,
 			   output wire [3:0] vgaGreen,
 			   output wire [3:0] vgaBlue,
 			   output wire	     hsync,
 			   output wire	     vsync
-			   );
 
+    );
+   wire [1:0] game_state;//Could be used in FSM functionality or something else to tell
+                          //when the game is supposed to be in 
+   wire [2:0] draw_bullet = 0;
    wire [9:0] hcount;
    wire [9:0] vcount;
    wire	      hblank;
    wire	      vblank;
    wire	      pixpulse;
-   wire	      is_a_wall, empty, draw_asteroid;
-//   wire	      draw_bigball;
-//   wire       draw_ball2,draw_ball3;
-   wire       draw_ship;
-   wire       draw_score;
-   wire [23:0] draw_b;
-   wire [23:0] broken_b;
-   reg [7:0] sc = 8'b00000000;
-   wire	      move;
+   wire       wall = ((hcount < 132) & (hcount > 137) & (hcount >408) & (hcount < 413));
+                      //Gives boundaries for thin wall on sides. Note that there aren't provisions
+                      //for walls on the top or bottom.
+   wire       empty;
+   
+   wire [9:0]       Ship_X, Ship_Y;
+      
+   wire       draw_ship,draw_score;
+   wire [7:0] draw_asteroids;//Arbitrary 4 bits, didn't think more than 16 could be on screen
+   wire [7:0] broken_asteroids;//Broken and draw for blocks was a package deal
+   
+   wire [7:0] timer_RNG;//Use for pseudo random calculations
+   wire [7:0] display = 8'b00000000;
+   
+   wire move;
    reg [11:0] current_pixel;
    reg vblank_d1;
-   wire all_broken;
-   wire empty_cond;
- //  wire move_up,move_down,move_left,move_right;
-
-   localparam SHIP_COLOR = 12'hf7b;     // pink color
-   localparam SCORE_COLOR = 12'hfff;    // white color
-   localparam BLOCK_COLOR = 12'hf0a;    // pink
-   localparam WALL_COLOR = 12'h00f;     // blue
-   localparam BALL_COLOR = 12'h0f0;     // Greem
-   localparam EMPTY_COLOR = 12'h000;    // black color
-   
-   //---------------------------------------------
+   wire [7:0] scores;
+   //
+   reg [3:0]  val;
+   wire [31:0] seed = 4;  // prefer 32b value
+   //
+   localparam SHIP_COLOR = 12'hfd0;//Yellow
+   localparam SCORE_COLOR = 12'hfff;//White
+   localparam BULLET_COLOR = 12'h0ff;//Turqouise
+   localparam WALL_COLOR = 12'hf00;//Red, Note: Make walls thin so we don't get flashbanged upon loading game
+   localparam AST_COLOR = 12'h850;//Brown
+   localparam EMPTY_COLOR = 12'h001;//Background, black with a very small amount of blue
+    
+    //---------------------------------------------
    // VGA Timing Generator
    //---------------------------------------------
    vga_timing vga_gen (
@@ -55,175 +67,99 @@ module top_vga(
 		       .hblank (hblank),
 		       .vblank (vblank)
 		       );
-   //---------------------------------------------
-   // Asteroid generation
-   //---------------------------------------------    
-    asteroid_small #(320, 240, 0, 0) asteroid1(
-        // outputs
-        .draw_asteroid(draw_asteroid),
-        .xloc(),
-        .yloc(),
-        // inputs
-        .clk(clk),
-        .pixpulse(pixpulse),
-        .rst(rst),
-        .hcount(hcount[9:0]),
-        .vcount(vcount[9:0]),
-		.empty((empty & ~draw_ship)),
-		.move(move)
-        );
-    
-//   asteroid_small #(100,30,0,0) u_ball_1 ( 
-//		  // Outputs
-//		  .draw_ball		(draw_ball),
-//		  .xloc			(),
-//		  .yloc			(),
-//		  // Inputs
-//		  .clk			(clk),
-//		  .pixpulse             (pixpulse),
-//		  .rst			(rst),
-//		  .hcount		(hcount[9:0]),
-//		  .vcount		(vcount[9:0]),
-//		  .empty		((empty & ~draw_bigball & ~draw_ship & ~draw_ball2 & ~draw_ball3)),
-//		  .move			(move));
-	  
-// asteroid_big #(200,90,0,0) u_bigball ( 
-//		  // Outputs
-//		  .draw_ball		(draw_bigball),
-//		  .xloc			(),
-//		  .yloc			(),
-//		  // Inputs
-//		  .clk			(clk),
-//		  .pixpulse             (pixpulse),
-//		  .rst			(rst),
-//		  .hcount		(hcount[9:0]),
-//		  .vcount		(vcount[9:0]),
-//		  .empty		(empty & ~draw_ball & ~draw_ship & ~draw_ball2 & ~draw_ball3),
-//		  .move			(move));
-
-   //---------------------------------------------
-   // Spaceship handling
-   //---------------------------------------------		  s
-spaceship #(375,440,0,0) u_ship(
-        //Outputs
-        .draw_ship    (draw_ship),
-        .xloc           (),
-        .yloc           (),
-        //Inputs
-        .clk			(clk),
-        .mU             (move_up),
-        .mD             (move_down),
-        .mL             (move_left),
-        .mR             (move_right),
-		  .pixpulse             (pixpulse),
-		  .rst			(rst),
-		  .hcount		(hcount[9:0]),
-		  .vcount		(vcount[9:0]),
-		  .empty		(empty & ~draw_ball & ~draw_bigball & ~draw_ball2 & ~draw_ball3),
-		  .move			(move));
-   //---------------------------------------------
-   // Score counter
-   //---------------------------------------------		  
-score #(80,420) s1(
-   
-    .clk (clk), // 100 MHz system clock
-    .pixpulse (pixpulse), // every 4 clocks for 25MHz pixel rate
+		       
+    ship # () ship1(
+    .clk    (clk),
+    .pixpulse (pixpulse),
     .rst (rst),
-   	.hcount(hcount[9:0]), // x-location where we are drawing
-    .vcount (vcount[9:0]), // y-location where we are drawing
-    .score (sc[7:0]),
-    .draw_score (draw_score)
+    .hcount(hcount),
+    .vcount(vcount),
+    .empty (empty), //Make sure to adjust for lives and collision with various objects, m
+    .move (move),                //Make sure that ship does not collide with bullet upon shooting
+    .mU (move_up),
+    .mD (move_down),
+    .mL (move_left),
+    .mR (move_right),
+    .draw_ship (draw_ship),
+    .xloc (Ship_X),
+    .yloc (Ship_Y)
     );
-
-/*  block #(120,100,20,10) b1
-   (
-    .clk (clk), // 100 MHz system clock
-    .pixpulse (pixpulse), // every 4 clocks for 25MHz pixel rate
-    .rst(rst),
-    .hcount(hcount[9:0]), // x-location where we are drawing
-    .vcount(vcount[9:0]), // y-location where we are drawing
-    .empty (1'b1) , // is this pixel empty
-    .move(move), // signal to update the status of the block
-    .unbreak(all_broken),  // reset the block
-    .draw_block(draw_b[0]), // is the block being drawn here?
-    .broken(broken_b[0]) // is this block broken
-    );
-    block #(150,100,20,10) b2
-   (
-    .clk (clk), // 100 MHz system clock
-    .pixpulse (pixpulse), // every 4 clocks for 25MHz pixel rate
-    .rst(rst),
-    .hcount(hcount[9:0]), // x-location where we are drawing
-    .vcount(vcount[9:0]), // y-location where we are drawing
-    .empty (1'b1) , // is this pixel empty
-    .move(move), // signal to update the status of the block
-    .unbreak(all_broken),  // reset the block
-    .draw_block(draw_b[0]), // is the block being drawn here?
-    .broken(broken_b[0]) // is this block broken
-    );*/
-//    genvar i;
-//    generate
-//    for (i = 0; i < 24; i = i + 1) begin : gen_blocks
-//            block #(
-//                .xloc(50 + (i % 12) * 45),
-//                .yloc(50 + (i / 12) * 30)
-//            ) u_block (
-//                .clk(clk),
-//                .pixpulse(pixpulse),
-//                .rst(rst),
-//                .hcount(hcount),
-//                .vcount(vcount),
-//                .empty(empty_cond),
-//                .move(move),
-//                .unbreak(all_broken),
-//                .draw_block(draw_b[i]),
-//                .broken(broken_b[i])
-//            );
-//        end
-//    endgenerate
-   //---------------------------------------------
-   // Generate border, screen size is 640 x 480
-   // border is going to be horizontal: 160-480 ;   verticle: 10-470;
-   //---------------------------------------------
-   assign is_a_wall = ((hcount < 160) | (hcount > 480) | (vcount < 10) | (vcount > 460));
-//assign is_a_wall2 = (((hcount<330)&(hcount>310) & (vcount<280)&(vcount>200)));//Island
-   
     
-   assign empty = ~(is_a_wall | (|draw_b[23:0]));
-   assign empty_cond = empty & ~draw_ball & ~draw_ship;
-assign all_broken = &broken_b;
-   assign move = (vblank & ~vblank_d1);  // move balls at start of vertical blanking
-reg [23:0] blk_next;
-   always @(posedge clk or posedge rst) begin
-      if (rst) begin
-	 vblank_d1 <= 0;
-	 sc = 0;
-      end else if (pixpulse) begin
-      if(blk_next ^ broken_b) begin
-      sc = sc+1;
-      end
-      
-         vblank_d1 <= vblank;
-         blk_next = broken_b;
-      end
-   end
-   
-   // Register the current pixel
-   always @(posedge clk) begin
-      if (pixpulse)
-        current_pixel <= (is_a_wall) ? 
-        WALL_COLOR : 
-         (draw_ball) ? BALL_COLOR :
-         (draw_ship) ? SHIP_COLOR: 
-        (|draw_b[23:0])? BLOCK_COLOR: 
-        (draw_score)? SCORE_COLOR:EMPTY_COLOR;
-   end
-   
 
-   // Map 12-bit to 4:4:4
-   assign vgaRed   = (~hblank && ~vblank) ? current_pixel[11:8] : 4'b0;
-   assign vgaGreen = (~hblank && ~vblank) ? current_pixel[7:4] : 4'b0;
-   assign vgaBlue  = (~hblank && ~vblank) ? current_pixel[3:0] : 4'b0;
-   
+
+score #() score1(
+    .clk (clk),
+    .pixpulse (pixpulse),
+    .rst (rst),
+    .hcount (hcount),
+    .vcount (vcount),
+    .score (scores),
+    .draw_score(draw_score)
+);
+    
+    //~~~~~~~~~~~~~~~~~~~~~~~~
+    // Generate 3 bullet
+    //~~~~~~~~~~~~~~~~~~~~~~~~
+    genvar i;
+    generate
+        for(i = 0; i < 3; i = i + 1) begin
+        
+            bullet #(Ship_X, Ship_Y) bullet_u
+            (
+    .clk        (clk),
+    .pixpulse   (pixpulse), 
+    .rst        (rst),
+    // bullet count change to check if bullet can fire
+    .bullet_count(), //If # of bullets is 2'b11, do not draw bullet /// might omit, main_VGA will track bullet amount
+    .hcount     (hcount), 
+    .vcount     (vcount),
+    .fire(~draw_bullet[i] && shoot), // check if we can fire this bullet max 3 bullet
+    .firePosX(Ship_X),
+    .firePosX(Ship_Y - 20),
+    .button_pressed      (shoot), //Needs to be incorporated into code, if statement or case statement
+    .empty      (empty & ~asteroid), 
+    .move       (move), // signal to update the location of the ball
+    .draw_bullet(draw_bullet[i]), //
+    .xloc(),
+    .yloc(),
+    .new_bullet_count(), // might not be used, draw_bullet is taking care 
+    .broken()
+    );
+    end
+    endgenerate 
+    //~~~~~~~~~~~~~~~~~~~~~~~~
+    // Generate 8 asteroid
+    //~~~~~~~~~~~~~~~~~~~~~~~~
+    always@(posedge clk)begin
+    val = $random(seed) % 271 + 137;
+    end
+    genvar j;
+    generate
+    for(j = 0; j < 3; i = i + 1) begin
+        asteroid # () asteroid_u(//Can adjust in top module to increase speed
+    .clk    (clk), 
+    .pixpulse(pixpulse), 
+    .rst(rst),
+    .hcount(hcount), 
+    .vcount(vcount),
+    .unbreak(),                //Adapted from 'block' module. Uncertain whether this will be used
+    .break_conditions(),//What did the asteroid collide with? 
+    .pre_score(), //Have the score be carried into asteroid module and have it be-
+    .empty(empty),       //-incremented by the module depending on what the asteroid hit.
+    .pre_lives(),  //See above comment, except for 'lives' of the ship
+    .move(move),      //Note: Need to have bus in top module for lives_next-
+    .draw_ast(), //-and other buses as to properly change value in following clk cycle
+    .xloc(), //Note: If we are including life tracking capabilities, I am not-
+    .yloc(),//-sure if checking for 0 lives happens here or in top module (probs latter)
+    .broken(),
+    .post_score(),
+    .post_lives(),
+    .inc_score()
+    );
+    end
+    endgenerate 
+    
+    assign vgaRed   = (~hblank && ~vblank) ? current_pixel[11:8] : 4'b0;
+    assign vgaGreen = (~hblank && ~vblank) ? current_pixel[7:4] : 4'b0;
+    assign vgaBlue  = (~hblank && ~vblank) ? current_pixel[3:0] : 4'b0;
 endmodule
-   
