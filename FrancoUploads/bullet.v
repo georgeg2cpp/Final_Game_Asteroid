e3f7ea51f4d084e205d3def8dd6c7c776d016ac2
Francology0 <79481739+Francology0@users.noreply.github.com>
2025-04-28 20:05:48 -0700

    Update bullet.v

--- FrancoUploads/bullet.v
+++ FrancoUploads/bullet.v
@@ -1,96 +1,131 @@
 `timescale 1ns / 1ps
 
 
-module bullet #(parameter xloc_start=320,
-	      parameter yloc_start=240,
-	      parameter ydir_start=1)(
-input	     clk,
+module asteroid # (parameter xloc_start = 320, //Adjust in top module with pseudo-RNG
+                   parameter yloc_start = 0,
+                   parameter xsize_div_2 = 10,//No need for separate module for large asteroid...
+                   parameter ysize_div_2 = 10,//This parameter gives half of the specified dimension on the asteroid
+                   parameter xdir_start = 0, //Can adjust parameter in top module to make it go diagonally
+                   parameter ydir_start = 1,
+                   parameter start_speed = 1)(//Can adjust in top module to increase speed
+    input	     clk, 
     input	     pixpulse, 
     input	     rst,
-    input      [1:0] bullet_count, //If # of bullets is 2'b11, do not draw bullet
     input [9:0]	     hcount, 
     input [9:0]	     vcount,
-    input button_pressed, //Needs to be incorporated into code, if statement or case statement
-    input	     empty, 
-    input	     move, // signal to update the location of the ball
-    output	     draw_bullet, //
-    output reg [9:0] xloc,
-    output reg [9:0] yloc,
-    output reg [1:0] new_bullet_count,
-    output reg broken 
-    );
+    input unbreak,                //Adapted from 'block' module. Uncertain whether this will be used
+    //input [1:0] break_conditions,//What did the asteroid collide with? 
+    //input [7:0] pre_score, //Have the score be carried into asteroid module and have it be-
+    input	     empty,       //-incremented by the module depending on what the asteroid hit.
+   // input [1:0] pre_lives,  //See above comment, except for 'lives' of the ship
+    input	     move,      //Note: Need to have bus in top module for lives_next-
     
-    reg [4:0]			 occupied_lft;
-   reg [4:0]			 occupied_rgt;
-   reg [4:0]			 occupied_bot;
-   reg [4:0]			 occupied_top;
-   reg				 xdir, ydir;
-   reg				 update_neighbors;
-   wire drawable;
-   wire				 blk_lft, blk_rgt;
-   wire				 blk_up, blk_dn;
-       assign drawable = ~(&bullet_count);
-       assign draw_bullet = (drawable & (hcount <= xloc+4) & (hcount >= xloc-4) & (vcount <= yloc+9) & (vcount >= yloc-9)) ?  ~broken : 0;
- always @(posedge clk or posedge rst)
+   
+    output	     draw_ast, //-and other buses as to properly change value in following clk cycle
+    output reg [9:0] xloc, //Note: If we are including life tracking capabilities, I am not-
+    output reg [9:0] yloc,//-sure if checking for 0 lives happens here or in top module (probs latter)
+    output reg broken,
+    //output reg [7:0] post_score,
+   // output reg [1:0] post_lives,
+    output reg inc_score
+    );
+    reg [5:0]			 occupied_lft;
+    reg [5:0]			 occupied_rgt;
+    reg [5:0]			 occupied_bot;
+    reg [5:0]			 occupied_top;
+    reg				 xdir, ydir;
+    reg				 update_neighbors;
+    
+    wire blk_lft = |occupied_lft;
+    wire blk_rgt = |occupied_rgt;
+    wire blk_bot = |occupied_bot;
+    
+    reg [2:0] speed = start_speed;
+    localparam END_SCREEN = 2'b00;
+    localparam HIT_BULLET = 2'b01;
+    localparam HIT_SHIP = 2'b10;
+    assign draw_ast = (hcount <= xloc+xsize_div_2) & (hcount >= xloc-xsize_div_2) & 
+			(vcount <= yloc+ysize_div_2) & (vcount >= yloc-ysize_div_2) ?  ~broken : 0;
+	always @(posedge clk or posedge rst)
      begin
 	if (rst) begin
-	   occupied_lft <= 5'b0;
-	   occupied_rgt <= 5'b0;
-	   occupied_bot <= 5'b0;
-	   occupied_top <= 5'b0;
-	end else if (pixpulse) begin  
-	   if (update_neighbors) begin
-	      occupied_lft <= 5'b0;
-	      occupied_rgt <= 5'b0;
-	      occupied_bot <= 5'b0;
-	      occupied_top <= 5'b0;
-	   end else if (~empty) begin
-	      if (vcount >= yloc-10 && vcount <= yloc+10) 
-		if (hcount == xloc+5)
-		  occupied_rgt[(yloc-vcount+10)] <= 1'b1;  
-		else if (hcount == xloc-5)
-		  occupied_lft[(yloc-vcount+10)] <= 1'b1;
+	   occupied_lft <= 0;
+	   occupied_rgt <= 0;
+	   occupied_bot <= 0;
+	   occupied_top <= 0;
+	end else if (pixpulse) begin  // only make changes when pixpulse is high
+	   if (vcount >= yloc-(ysize_div_2+1) && vcount <= yloc+(ysize_div_2+1)) 
+	     if (hcount == xloc+(xsize_div_2+1))
+	       occupied_rgt[(yloc-vcount+(ysize_div_2+1))] <= ~empty;  // LSB is at bottom
+	     else if (hcount == xloc-(xsize_div_2+1))
+	       occupied_lft[(yloc-vcount+(ysize_div_2+1))] <= ~empty;
 	      
-	      if (hcount >= xloc-5 && hcount <= xloc+5) 
-		if (vcount == yloc+10)
-		  occupied_bot[(xloc-hcount+5)] <= 1'b1;  
-		else if (vcount == yloc-10)
-		  occupied_top[(xloc-hcount+5)] <= 1'b1;
-	   end
+	   if (hcount >= xloc-(xsize_div_2+1) && hcount <= xloc+(xsize_div_2+1)) 
+	     if (vcount == yloc+(ysize_div_2+1))
+	       occupied_bot[(xloc-hcount+(xsize_div_2+1))] <= ~empty;  // LSB is at right
+	     else if (vcount == yloc-(ysize_div_2+1))
+	       occupied_top[(xloc-hcount+(xsize_div_2+1))] <= ~empty;
 	end
      end	      
-     assign blk_lft = |occupied_lft; 
-   assign blk_rgt = |occupied_rgt;  
-
-   assign blk_up = |occupied_top; 
-   assign blk_dn = |occupied_bot;
-     
-     
-    always @(posedge clk or posedge rst)
+     always @(posedge clk or posedge rst)
      begin
 	if (rst) begin
-	   xloc <= xloc_start;
-	   yloc <= yloc_start;
-	   //xdir <= xdir_start;
-	   ydir <= ydir_start;
-	   new_bullet_count <= 2'b00;
 	   broken <= 0;
-	   update_neighbors <= 0;
+	   inc_score <= 0;
+	   speed = start_speed;
 	end else if (pixpulse) begin
-	   update_neighbors <= 0; 
-	   if(~&bullet_count) broken <= 0;
+	
+	
+	   if (unbreak) begin
+	      broken <= 0;//Possibly put asteroid limit for how many can be on screen at a time
+	   end            //Could increase as time goes on...
 	   if (move) begin
-            if((blk_up) | (blk_lft) | (|blk_rgt)) begin
-                new_bullet_count = bullet_count - 1;
-                broken <= 1;
-                end
-            else begin
-            yloc <= yloc - 2;
-            end
-            
-//PUT IN CODE TO ACCOUNT FOR BUTTON PRESS
-	      update_neighbors <= 1;
+	      // inc_score <= 0;
+	           if((yloc+ysize_div_2+1) <= 10) begin
+		          yloc <= 60;//(ysize_div_2)+(ysize_div_2)+5;//Moves asteroid to top of screen
+		          speed <= speed + 1;
+		        //To-Do: Put code to increase the speed
+		        end
+	      if (blk_lft | blk_bot | blk_rgt) begin
+		      broken <= 1;//Could assign as broken, or set position to top of screen for asteroid
+		                  //to keep falling down until it collides with something else
+		                  //These comments also apply to 'END_SCREEN' case statement below
+		      //inc_score <= 1;  
+		        
+		        
+		        /*  case (break_conditions)
+		              END_SCREEN: begin
+		                  
+		 
+		 
+		                          end
+		              HIT_BULLET: begin
+		              post_score = pre_score + 1;
+		              
+		              
+		                          end
+		              HIT_SHIP:begin
+		              post_lives = pre_lives - 1;
+		              
+		                      end
+		              
+		          endcase*/
+		          
+	      end
+	      else begin
+	           yloc <= yloc + speed;
+	      end
+	      if(broken) begin
+	           broken <= 0;
+	           inc_score <= 1;
+	           //xloc = random between a and b 
+	           //yloc = 60
+	           //speed = speed + 1 if(score high enough) or enough time passes
+	
+	end
 	   end 
 	end 
-     end
+     end		
+			
+		
 endmodule
