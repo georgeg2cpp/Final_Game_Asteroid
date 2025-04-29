38e9c6f4522c2e4ce51e5726b2ee2db9363032b6
Francology0 <79481739+Francology0@users.noreply.github.com>
2025-04-28 20:06:35 -0700

    Update asteroid.v

--- FrancoUploads/asteroid.v
+++ FrancoUploads/asteroid.v
@@ -6,24 +6,28 @@ module asteroid # (parameter xloc_start = 320, //Adjust in top module with pseud
                    parameter xsize_div_2 = 10,//No need for separate module for large asteroid...
                    parameter ysize_div_2 = 10,//This parameter gives half of the specified dimension on the asteroid
                    parameter xdir_start = 0, //Can adjust parameter in top module to make it go diagonally
-                   parameter ydir_start = 1)(//Can adjust in top module to increase speed
+                   parameter ydir_start = 1,
+                   parameter start_speed = 1)(//Can adjust in top module to increase speed
     input	     clk, 
     input	     pixpulse, 
     input	     rst,
     input [9:0]	     hcount, 
     input [9:0]	     vcount,
     input unbreak,                //Adapted from 'block' module. Uncertain whether this will be used
-    input [1:0] break_conditions,//What did the asteroid collide with? 
-    input [7:0] pre_score, //Have the score be carried into asteroid module and have it be-
+    //input [1:0] break_conditions,//What did the asteroid collide with? 
+    //input [7:0] pre_score, //Have the score be carried into asteroid module and have it be-
     input	     empty,       //-incremented by the module depending on what the asteroid hit.
-    input [1:0] pre_lives,  //See above comment, except for 'lives' of the ship
+   // input [1:0] pre_lives,  //See above comment, except for 'lives' of the ship
     input	     move,      //Note: Need to have bus in top module for lives_next-
+    
+   
     output	     draw_ast, //-and other buses as to properly change value in following clk cycle
     output reg [9:0] xloc, //Note: If we are including life tracking capabilities, I am not-
     output reg [9:0] yloc,//-sure if checking for 0 lives happens here or in top module (probs latter)
     output reg broken,
-    output reg [7:0] post_score,
-    output reg [1:0] post_lives
+    //output reg [7:0] post_score,
+   // output reg [1:0] post_lives,
+    output reg inc_score
     );
     reg [5:0]			 occupied_lft;
     reg [5:0]			 occupied_rgt;
@@ -31,9 +35,12 @@ module asteroid # (parameter xloc_start = 320, //Adjust in top module with pseud
     reg [5:0]			 occupied_top;
     reg				 xdir, ydir;
     reg				 update_neighbors;
+    
     wire blk_lft = |occupied_lft;
     wire blk_rgt = |occupied_rgt;
     wire blk_bot = |occupied_bot;
+    
+    reg [2:0] speed = start_speed;
     localparam END_SCREEN = 2'b00;
     localparam HIT_BULLET = 2'b01;
     localparam HIT_SHIP = 2'b10;
@@ -64,16 +71,29 @@ module asteroid # (parameter xloc_start = 320, //Adjust in top module with pseud
      begin
 	if (rst) begin
 	   broken <= 0;
+	   inc_score <= 0;
+	   speed = start_speed;
 	end else if (pixpulse) begin
+	
+	
 	   if (unbreak) begin
 	      broken <= 0;//Possibly put asteroid limit for how many can be on screen at a time
 	   end            //Could increase as time goes on...
 	   if (move) begin
+	      // inc_score <= 0;
+	           if((yloc+ysize_div_2+1) <= 10) begin
+		          yloc <= 60;//(ysize_div_2)+(ysize_div_2)+5;//Moves asteroid to top of screen
+		          speed <= speed + 1;
+		        //To-Do: Put code to increase the speed
+		        end
 	      if (blk_lft | blk_bot | blk_rgt) begin
 		      broken <= 1;//Could assign as broken, or set position to top of screen for asteroid
 		                  //to keep falling down until it collides with something else
 		                  //These comments also apply to 'END_SCREEN' case statement below
-		          case (break_conditions)
+		      //inc_score <= 1;  
+		        
+		        
+		        /*  case (break_conditions)
 		              END_SCREEN: begin
 		                  
 		 
@@ -83,18 +103,29 @@ module asteroid # (parameter xloc_start = 320, //Adjust in top module with pseud
 		              post_score = pre_score + 1;
 		              
 		              
-		              end
+		                          end
 		              HIT_SHIP:begin
 		              post_lives = pre_lives - 1;
 		              
-		              end
+		                      end
 		              
-		          endcase
+		          endcase*/
+		          
 	      end
+	      else begin
+	           yloc <= yloc + speed;
+	      end
+	      if(broken) begin
+	           broken <= 0;
+	           inc_score <= 1;
+	           //xloc = random between a and b 
+	           //yloc = 60
+	           //speed = speed + 1 if(score high enough) or enough time passes
+	
+	end
 	   end 
 	end 
      end		
 			
-			//CODE FOR WHETHER THE ASTEROID IS HIT BY A BULLET, BY THE SHIP, OR REACHes
-			//BOTTOM OF SCREEN
+		
 endmodule
