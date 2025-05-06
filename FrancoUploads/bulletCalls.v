9b95dfc0a541deed556372a2185d7a911b5ccd32
Francology0 <79481739+Francology0@users.noreply.github.com>
2025-05-05 17:08:19 -0700

    Create bulletCalls.v

---
+++ FrancoUploads/bulletCalls.v
@@ -0,0 +1,36 @@
+//   bullet #(450,240,4,9) u_bullet1(
+//        .clk (clk),
+//        .pixpulse (pixpulse),
+//        .rst (rst),
+//        .hcount (hcount),
+//        .vcount (vcount),
+//        .empty (empty & ~draw_ball & ~draw_ball2 & ~draw_ball3),
+//        .other (1'b1),
+//        .move (move),
+//        .press (shoot),
+//        .ship_x (ship_x),
+//        .ship_y (ship_y),
+//        .draw_bullet (draw_bullet1),
+//        .xloc (),
+//        .yloc (),
+//        .inc_score () 
+    
+//    );
+//    bullet #(100,240,4,9) u_bullet2(
+//        .clk (clk),
+//        .pixpulse (pixpulse),
+//        .rst (rst),
+//        .hcount (hcount),
+//        .vcount (vcount),
+//        .empty (empty & ~draw_ball & ~draw_ball2 & ~draw_ball3),
+//        .other (draw_bullet1),
+//        .move (move),
+//        .press (shoot),
+//        .ship_x (ship_x),
+//        .ship_y (ship_y),
+//        .draw_bullet (draw_bullet2),
+//        .xloc (),
+//        .yloc (),
+//        .inc_score () 
+    
+//    );
